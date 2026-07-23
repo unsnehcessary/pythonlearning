bfe0f68e1fd7bdb47abcb3d2d8edd718af2e79d7
unsnehcessary <whysnehaa@gmail.com>
2026-07-23 18:28:56 +0530

    grade_calculator.py

---
+++ grade_calculator.py
@@ -0,0 +1,13 @@
+marks = int(input("How much did you score? "))
+
+if marks >= 90:
+    print("You got an A")
+
+elif marks >= 75:
+    print("You got a B")
+
+elif marks >= 40:
+    print("You got a C")
+
+else:
+    print("You failed")
